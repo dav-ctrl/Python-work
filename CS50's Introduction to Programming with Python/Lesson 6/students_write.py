216e1684fb4c453fd0e9610df404265b49da0b0b
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-09-08 16:42:16 +0200

    Create students_write.py

---
+++ CS50's Introduction to Programming with Python/Lesson 6/students_write.py
@@ -0,0 +1,14 @@
+import csv
+
+name = input("What's your name? ")
+home = input("What's your home? ")
+
+with open("students_write.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
+
+#
+
+with open("students_write.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
