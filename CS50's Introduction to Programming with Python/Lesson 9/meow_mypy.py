02a2b2f0dc3144a3bcfa2870c6f5a850799acea5
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-09-08 17:06:46 +0200

    Create meow_mypy.py

---
+++ CS50's Introduction to Programming with Python/Lesson 9/meow_mypy.py
@@ -0,0 +1,17 @@
+# Wrong
+def meow(n: int) -> None:
+    for _ in range(n):
+        print("meow")
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows)
+
+# Right
+
+def meow(n: int) -> str:
+    return "meow\n" * n
+
+number: int = int(input("Number: "))
+meows: str = meow(number)
+print(meows, end="")
