74153b3c570d85a5a63cae2f375b0ec0f97927c3
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-09-08 13:33:07 +0200

    Create deep.py

---
+++ CS50's Introduction to Programming with Python/Problem Set 1/deep.py
@@ -0,0 +1,6 @@
+word = input("What is the Answer to the Great Question of Life, the Universe, and the Everything? ")
+
+if word == "42" or word == "forty-two" or word == "forty two":
+    print("Yes")
+else:
+    print("No")
